b4782383c6fd76b28cc0ca34fff9d24669c4563b
Tammy Metz <tammy@tammymetz.com>
2019-08-22 15:53:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ function retrieveEmployeeInformation() {
   return input.value;
 }
 
+function addNewElementAsLi() {
+  let list = document.querySelector('.employee-list');
+  let name = retrieveEmployeeInformation();
+  list.insertAdjacentHTML('beforeend', '<li> ${name} </li>');
+}
+
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
