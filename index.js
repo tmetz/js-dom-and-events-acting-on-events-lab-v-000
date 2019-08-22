fab1d76ccdedf967da77101edad57af864127e18
Tammy Metz <tammy@tammymetz.com>
2019-08-22 16:03:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+let input = document.querySelector('input');
+
+function retrieveEmployeeInformation() {
+  return input.value;
+}
+
+function addNewElementAsLi() {
+  let list = document.querySelector('.employee-list');
+  let name = retrieveEmployeeInformation();
+  list.insertAdjacentHTML('beforeend', `<li> ${name} </li>`);
+}
+
+function resetInput(){
+  document.querySelector('input').value = ''
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]');
+  submit.addEventListener('click', function(event){
+    addNewElementAsLi();
+    resetInput();
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  let clearLink = document.querySelector('a');
+  clearLink.addEventListener('click', function(event) {
+    let ordered = document.querySelector('ul');
+    ordered.innerHTML = '';
+  })
+}
+
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
